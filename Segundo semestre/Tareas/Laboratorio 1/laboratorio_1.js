34387c648525953cf302b8f8aea21b411202b888
EddieGilXD <djuntaied@gmail.com>
2024-10-03 23:26:32 -0600

    Tarea de laboratorio 1 terminada y subida

---
+++ Segundo semestre/Tareas/Laboratorio 1/laboratorio_1.js
@@ -0,0 +1,55 @@
+let canciones = [
+    {
+        titulo: "Billie Jean",
+        artista: "Michael Jackson",
+        año: 1982
+    },
+    {
+        titulo: "Money",
+        artista: "Pink Floyd",
+        año: 1973
+    },
+    {
+        titulo: "Back in Black",
+        artista: "AC/DC",
+        año: 1980
+    },
+    {
+        titulo: "Go Your Own Way",
+        artista: "Fleetwood Mac",
+        año: 1977
+    },
+    {
+        titulo: "While My Guitar Gently Weeps",
+        artista: "The Beatles",
+        año: 1968
+    },
+    {
+        titulo: "Smells Like Teen Spirit",
+        artista: "Nirvana",
+        año: 1991
+    },
+    {
+        titulo: "The Real Slim Shady",
+        artista: "Eminem",
+        año: 2000
+    },
+    {
+        titulo: "Someone Like You",
+        artista: "Adele",
+        año: 2011
+    },
+    {
+        titulo: "Come Together",
+        artista: "The Beatles",
+        anio: 1969
+    },
+    {
+        titulo: "Dancing in the Dark",
+        artista: "Bruce Springsteen",
+        anio: 1984
+    }
+];
+
+canciones.forEach(canciones => {console.log(`Título: ${canciones.titulo}, Artista: ${canciones.artista}, Año: ${canciones.año}`);
+});
