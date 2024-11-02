6a078c8a5833c7d806a11beaad9423f82ab2f052
EddieGilXD <djuntaied@gmail.com>
2024-11-02 10:13:33 -0600

    Reparando el codigo
    Era simple mente que no estaba definiendo bien mis variables

--- Segundo semestre/Tareas/Tarea 2/laboratorio1.js
+++ Segundo semestre/Tareas/Tarea 2/laboratorio1.js
@@ -5,19 +5,27 @@ class User {
         this.surname = surname;
         this.email = email;
         this.role = role;
+        this.courses = [];        
     }
 
-    addCourse(course, level) {
-        this.course = course;
-        this.level = level;
+    addCourse(course, level){        
+        this.courses[course] = level;               
     }
 
-    removeCourse() {
-        this.course = null;
-        this.level = null;
+    removeCourse(course) {
+        if (this.courses[course]) {
+            delete this.courses[course];            
+        } else {console.log("Error no hay curso a eliminar");}        
     }
+    
 }
 
 let user1 = new User ("Pablo", "Flores", "test@test.com", "Maestro");
 console.log(user1.name, user1.surname, user1.email, user1.role);
-user1.addCourse("Matematicas", 3)
+
+user1.addCourse("Matematicas", 3);
+console.log(user1.courses);
+
+user1.removeCourse("Matematicas");
+console.log(user1);
+
