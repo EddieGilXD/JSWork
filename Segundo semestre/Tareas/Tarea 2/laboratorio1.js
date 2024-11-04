ab9f7f471994395f20d58964a55870f24d68f46e
EddieGilXD <djuntaied@gmail.com>
2024-11-03 21:06:02 -0600

    refactorizado

--- Segundo semestre/Tareas/Tarea 2/laboratorio1.js
+++ Segundo semestre/Tareas/Tarea 2/laboratorio1.js
@@ -1,4 +1,4 @@
-export class User {
+class User {
 
     constructor ({name, surname, email, role}){
         this.name = name;
@@ -46,19 +46,54 @@ export class User {
     }
 }
 
-let student1 = new User({name: 'Rafael', surname: 'Fife', email: 'rfife@rhyta.com', role: 'student'});
-let student2 = new User({name: 'Kelly', surname: 'Estes', email: 'k_estes@dayrep.com', role: 'student'});
-let teacher1 = new User({name: 'Paula', surname: 'Thompkins', email: 'PaulaThompkins@jourrapide.com', role: 'teacher'});
+// /*let student1 = new User({name: 'Rafael', surname: 'Fife', email: 'rfife@rhyta.com', role: 'student'});*/
+// let student2 = new User({name: 'Kelly', surname: 'Estes', email: 'k_estes@dayrep.com', role: 'student'});
+// let teacher1 = new User({name: 'Paula', surname: 'Thompkins', email: 'PaulaThompkins@jourrapide.com', role: 'teacher'});
 
-student1.addCourse('maths', 2);
-student1.addCourse('physics', 1);
-student1.removeCourse('physics');
-teacher1.addCourse('biology', 3);
-teacher1.editCourse('biology', 4);
-console.log(`${student1.name}: ${student1.courses.length} courses`); // -> Rafael: 1 courses
-console.log(`${teacher1.name}: ${teacher1.courses.length} courses`); // -> Paula: 1 courses
-teacher1.sendMessage(student1, 'test message');
-teacher1.sendMessage(student1, 'another message');
-teacher1.showMessagesHistory();
+// student1.addCourse('maths', 2);
+// student1.addCourse('physics', 1);
+// student1.removeCourse('physics');
+// teacher1.addCourse('biology', 3);
+// teacher1.editCourse('biology', 4);
+// console.log(`${student1.name}: ${student1.courses.length} courses`); // -> Rafael: 1 courses
+// console.log(`${teacher1.name}: ${teacher1.courses.length} courses`); // -> Paula: 1 courses
+// teacher1.sendMessage(student1, 'test message');
+// teacher1.sendMessage(student1, 'another message');
+// teacher1.showMessagesHistory();
 
 /*Tarea terminada finalmente*/
+class ExtendedUser extends User {
+    get fullName() {
+        return `${this.name} ${this.surname}`;
+    }
+
+    set fullName(value) {
+        let [firstName, lastName] = value.split(' ');
+        this.name = firstName;
+        this.surname = lastName;
+    }
+}
+
+class Teacher extends ExtendedUser {
+    constructor({name, surname, email}) {
+        super({name, surname, email, role: "teacher"});
+    }
+}
+class Student extends ExtendedUser {
+    constructor({name, surname, email}){
+        super({name, surname, email, role: "student"})
+    }
+}
+
+
+let student1 = new Student({name: 'Rafael', surname: 'Fife', email: 'rfife@rhyta.com'});
+let student2 = new Student({name: 'Kelly', surname: 'Estes', email: 'k_estes@dayrep.com'});
+let teacher1 = new Teacher({name: 'Paula', surname: 'Thompkins', email: 'PaulaThompkins@jourrapide.com'});
+
+student1.addCourse('maths', 2);
+teacher1.addCourse('biology', 3);
+teacher1.editCourse('chemistry', 4);
+console.log(`${student1.fullName}: ${student1.courses.length} courses`); 
+console.log(`${teacher1.fullName}: ${teacher1.courses.length} courses`); 
+student1.fullName = 'Rafael Fifer';
+console.log(`${student1.fullName}: ${student1.courses.length} courses`); 
---
+++ Segundo semestre/Tareas/Tarea 2/readme.md
@@ -0,0 +1,1 @@
+Todas las tareas se encuentran bajo laboratorio 1 con fines de testeo
